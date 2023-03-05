29b03f3f2b8e506c6f4a6313d64d3a0ac82e000c
shreekhand.wankhede <shreekhand.wankhede@gmail.com>
2023-03-05 14:26:03 +0530

    Spring Boot with OAthu2 example - initial commit

---
+++ src/test/java/com/example/springbootoauth2example/SpringbootOauth2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootoauth2example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauth2ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
